bbedc1c62dca4e1e5290de745fd83c97408b37c4
in1dw3 <in1dw3@HZ352020.elorrieta.int>
2023-02-01 17:06:26 +0100

    cine pojo

--- CajeroEntradas/src/pojos/Cine.java
+++ CajeroEntradas/src/pojos/Cine.java
@@ -1,5 +1,26 @@
 package CajeroEntrada.pojos;
 
 public class Cine {
+	private int cod = 0;
+	
+	
+	private String nombre = null;
+	private String direccion = null;
+	private int NoSalas = 0;
+	
+	ArrayList<Sala> salas;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
