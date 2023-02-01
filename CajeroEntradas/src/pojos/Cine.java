fc059e6540b36fca640da9c9f62890c98760023d
in1dw3 <in1dw3@HZ352021.elorrieta.int>
2023-02-01 19:16:38 +0100

    Clase Cliente creada

--- CajeroEntradas/src/pojos/Cine.java
+++ CajeroEntradas/src/pojos/Cine.java
@@ -9,54 +9,9 @@ public class Cine implements Serializable {
 	 */
 	private static final long serialVersionUID = 565880329585090674L;
 
-
 	private int cod = 0;
-	
 	
 	private String nombre = null;
 	private String direccion = null;
-	private int NoSalas = 0;
-	
-	ArrayList<Sala> salas;
-
-	public int getCod() {
-		return cod;
-	}
-
-	public void setCod(int cod) {
-		this.cod = cod;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getDireccion() {
-		return direccion;
-	}
-
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
-
-	public int getNoSalas() {
-		return NoSalas;
-	}
-
-	public void setNoSalas(int noSalas) {
-		NoSalas = noSalas;
-	}
-
-	public ArrayList<Sala> getSalas() {
-		return salas;
-	}
-
-	public void setSalas(ArrayList<Sala> salas) {
-		this.salas = salas;
-	}
 		
 }
---
+++ CajeroEntradas/src/pojos/Cliente.java
@@ -0,0 +1,21 @@
+package pojos;
+
+import java.io.Serializable;
+
+public class Cliente implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3058116800081599783L;
+	
+	private String dni = null;
+	
+	private String nombre = null;
+	private String apellidos = null;
+	private String sexo = null;
+	private String contrasena = null;
+	private int tfno = 0;
+	private String direccion = null;
+	private String email = null;
+}
