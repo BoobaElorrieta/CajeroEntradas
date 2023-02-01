e8aa54946317b5a9f33830f73e9d35fde21b7fbe
in1dw3 <in1dw3@HZ352020.elorrieta.int>
2023-02-01 19:30:00 +0100

    pojo sala

--- CajeroEntradas/src/pojos/Sala.java
+++ CajeroEntradas/src/pojos/Sala.java
@@ -1,5 +1,25 @@
 package pojos;
 
-public class Sala {
+import java.io.Serializable;
+import java.util.ArrayList;
 
+
+
+public class Sala implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3596305966431711984L;
+
+	private int cod = 0;
+	
+	private String Nombre = null;
+	private String Pelicula = null;
+	
+	ArrayList<Pelicula> peliculas;
+	Cine cine =null;
+	
+	
+	
 }
