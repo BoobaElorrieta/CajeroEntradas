68afd0afef3ea1f4fc9953ada95c3707de9a893b
in1dw3 <in1dw3@HZ352020.elorrieta.int>
2023-02-01 17:44:04 +0100

    cine y sala pojo

--- CajeroEntradas/src/pojos/Cine.java
+++ CajeroEntradas/src/pojos/Cine.java
@@ -1,4 +1,6 @@
-package CajeroEntrada.pojos;
+package pojos;
+
+import java.util.ArrayList;
 
 public class Cine {
 	private int cod = 0;
@@ -9,12 +11,52 @@ public class Cine {
 	private int NoSalas = 0;
 	
 	ArrayList<Sala> salas;
+
+	public int getCod() {
+		return cod;
+	}
+
+	public void setCod(int cod) {
+		this.cod = cod;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public int getNoSalas() {
+		return NoSalas;
+	}
+
+	public void setNoSalas(int noSalas) {
+		NoSalas = noSalas;
+	}
+
+	public ArrayList<Sala> getSalas() {
+		return salas;
+	}
+
+	public void setSalas(ArrayList<Sala> salas) {
+		this.salas = salas;
+	}
 	
 	
 	
 	
 	
-	fasfgasg
+	
 	
 	
 	
