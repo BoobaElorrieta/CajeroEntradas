6b0039c392202b36d1bf1efcb0edeea05b0eabc9
in1dw3 <in1dw3@HZ352021.elorrieta.int>
2023-02-02 17:51:26 +0100

    Creo que todos los POJOs con todo bien creado

--- CajeroEntradas/src/pojos/Cliente.java
+++ CajeroEntradas/src/pojos/Cliente.java
@@ -23,5 +23,107 @@ public class Cliente implements Serializable{
 	
 	ArrayList <Entrada>entradas;
 
-	 
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+	public int getTfno() {
+		return tfno;
+	}
+
+	public void setTfno(int tfno) {
+		this.tfno = tfno;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public ArrayList<Entrada> getEntradas() {
+		return entradas;
+	}
+
+	public void setEntradas(ArrayList<Entrada> entradas) {
+		this.entradas = entradas;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(apellidos, contrasena, direccion, dni, email, entradas, nombre, sexo, tfno);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(apellidos, other.apellidos) && Objects.equals(contrasena, other.contrasena)
+				&& Objects.equals(direccion, other.direccion) && Objects.equals(dni, other.dni)
+				&& Objects.equals(email, other.email) && Objects.equals(entradas, other.entradas)
+				&& Objects.equals(nombre, other.nombre) && Objects.equals(sexo, other.sexo) && tfno == other.tfno;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [dni=" + dni + ", nombre=" + nombre + ", apellidos=" + apellidos + ", sexo=" + sexo
+				+ ", contrasena=" + contrasena + ", tfno=" + tfno + ", direccion=" + direccion + ", email=" + email
+				+ ", entradas=" + entradas + "]";
+	}
+
 }
--- CajeroEntradas/src/pojos/Entrada.java
+++ CajeroEntradas/src/pojos/Entrada.java
@@ -2,6 +2,8 @@ package pojos;
 
 import java.io.Serializable;
 import java.util.Objects;
+import java.util.Date;
+
 
 public class Entrada implements Serializable{
 	
@@ -12,8 +14,59 @@ public class Entrada implements Serializable{
 
 	private int cod = 0;
 	
+	private Date fechaDeCompra = null;
 	
-	
-	
+	Cliente cliente = null;
+
+	public int getCod() {
+		return cod;
+	}
+
+	public void setCod(int cod) {
+		this.cod = cod;
+	}
+
+	public Date getFechaDeCompra() {
+		return fechaDeCompra;
+	}
+
+	public void setFechaDeCompra(Date fechaDeCompra) {
+		this.fechaDeCompra = fechaDeCompra;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cliente, cod, fechaDeCompra);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Entrada other = (Entrada) obj;
+		return Objects.equals(cliente, other.cliente) && cod == other.cod
+				&& Objects.equals(fechaDeCompra, other.fechaDeCompra);
+	}
+
+	@Override
+	public String toString() {
+		return "Entrada [cod=" + cod + ", fechaDeCompra=" + fechaDeCompra + ", cliente=" + cliente + "]";
+	}
 
 }
