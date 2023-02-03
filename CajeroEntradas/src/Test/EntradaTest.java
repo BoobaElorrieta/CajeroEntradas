48cd1fbbfdb25092be2f3a6293e8a8104261bfa9
in1dw3 <in1dw3@HZ352020.elorrieta.int>
2023-02-03 17:14:28 +0100

    Entrada JUnit text mas o menos hecho

---
+++ CajeroEntradas/src/Test/EntradaTest.java
@@ -0,0 +1,38 @@
+package Test;
+
+import static org.junit.Assert.*;
+import java.io.Serializable;
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+import Modelo.pojos.Entrada;
+
+class EntradaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testSerailizable() {
+		Entrada entrada= new Entrada();
+		assertTrue("No se puede realizar la serializacion!!!", entrada instanceof Serializable);
+	}
+	@Test
+	public void testcod() {
+		Entrada entrada = new Entrada();
+		int cod= 13;
+		entrada.setCod(cod);
+		assertEquals("Cod no esta accesible", cod, entrada.getCod());
+	}
+	
+	@Test
+	public void testfechaDeCompra() {
+		Entrada entrada = new Entrada();
+		Date fechaDeCompra=  2012-12-12;
+		entrada.setFechaDeCompra(fechaDeCompra);
+		assertNotEquals("fechaDeCompra no esta accesible", fechaDeCompra, entrada.getFechaDeCompra());
+	}
+	
+	
+}
