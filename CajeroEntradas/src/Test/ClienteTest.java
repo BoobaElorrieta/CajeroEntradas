862e8d2a77a8a54f8da7fa5f569c1efb926f5e78
ibaiv <ibaiv@PortatilDeIbai>
2023-02-03 19:18:57 +0100

    ArreglosNuevos

--- CajeroEntradas/src/Test/ClienteTest.java
+++ CajeroEntradas/src/Test/ClienteTest.java
@@ -3,10 +3,12 @@ package Test;
 import static org.junit.Assert.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 
 import Modelo.pojos.Cliente;
+import Modelo.pojos.Entrada;
 import Modelo.pojos.Pelicula;
 
 class ClienteTest {
@@ -37,8 +39,100 @@ class ClienteTest {
 	@Test
 	public void testApellidos() {
 		Cliente cliente = new Cliente();
-		String nombre = "Maria";
-		cliente.setNombre(nombre);
-		assertNotEquals("Nombre no está accesible", nombre, cliente.getNombre());
+		String apellido = "Gimenez";
+		cliente.setApellidos(apellido);
+		assertNotEquals("Nombre no está accesible", apellido, cliente.getApellidos());
+	}
 	
+	@Test
+	public void testSexo() {
+		Cliente cliente = new Cliente();
+		String sexo = "Hombre";
+		cliente.setSexo(sexo);
+		assertNotEquals("Nombre no está accesible", sexo, cliente.getSexo());
+	}
+	
+	@Test
+	public void testContraseña() {
+		Cliente cliente = new Cliente();
+		String contraseña = "abc17384CBA";
+		cliente.setContrasena(contraseña);
+		assertNotEquals("Nombre no está accesible", contraseña, cliente.getContrasena());
+	}
+	
+	@Test
+	public void testTfno() {
+		Cliente cliente = new Cliente();
+		int tfno = 99999991;
+		cliente.setTfno(tfno);
+		assertNotEquals("Id no está accesible", tfno, cliente.getTfno());
+	}
+	
+	@Test
+	public void testDireccion() {
+		Cliente cliente = new Cliente();
+		String direccion = "C/Lehendakari Aguirre 179";
+		cliente.setDireccion(direccion);
+		assertNotEquals("Nombre no está accesible", direccion, cliente.getDireccion());
+	}
+	
+	@Test
+	public void testEmail() {
+		Cliente cliente = new Cliente();
+		String email = "elorritea@gmail.com";
+		cliente.setEmail(email);
+		assertNotEquals("Nombre no está accesible", email, cliente.getEmail());
+	}
+	
+	
+	@Test
+	public void testEntradas() {
+		Cliente cliente = new Cliente();
+		ArrayList<Entrada> entradas = null;
+		cliente.setEntradas(entradas);
+		assertNotEquals("Nombre no está accesible", entradas, cliente.getEmail());
+	}
+	
+	@Test
+	public void testClienteExiste() {
+		Cliente cliente = new Cliente();
+		cliente.setTfno(1234);
+		cliente.setDni("Zipi");
+		Cliente cliente2 = new Cliente();
+		cliente2.setTfno(1234);
+		cliente2.setDni("Zipi");
+
+		assertEquals("usuarios no son iguales!!!!", cliente, cliente2);
+	}
+
+	@Test
+	public void testClienteNoExiste() {
+		Cliente cliente = new Cliente();
+		cliente.setTfno(1234);
+		cliente.setDni("Zipi");
+		Cliente cliente2 = new Cliente();
+		cliente2.setTfno(1234);
+		cliente2.setDni("Zipi");
+
+		assertNotEquals("usuarios son iguales!!!!", cliente, cliente2);
+
+	}
+
+	@Test
+	public void testToString() {
+		Cliente cliente = new Cliente();
+		cliente.setDni("Hola");
+		cliente.setNombre("Hola");
+		cliente.setApellidos("Hola");
+		cliente.setSexo("Hola");
+		cliente.setContrasena("Hola");
+		cliente.setTfno(123);
+		cliente.setDireccion("Hola");
+		cliente.setEmail("Hola");
+		cliente.setEntradas(null);
+		String expected = "Cliente [Dni=" + cliente.getDni() + ", Nombre=" + cliente.getNombre() + ", Apellidos="
+				+ cliente.getApellidos() + ", Sexo=" + cliente.getSexo() + ", Contraseña=" + cliente.getContrasena()
+				+ ", Tlfn=" + cliente.getTfno() + ", Direccion=" + cliente.getDireccion()
+				+", Email=" + cliente.getEmail() + ", Entradas=" + cliente.getEntradas() + "]";
+	}
 }
