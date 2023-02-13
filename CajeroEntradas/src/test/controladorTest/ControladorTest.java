a25b1721c726bff381cc00e3b627ef89579d58b1
in1dw3 <in1dw3@HZ352021.elorrieta.int>
2023-02-13 16:16:05 +0100

    arraylist a public

--- CajeroEntradas/src/test/controladorTest/ControladorTest.java
+++ CajeroEntradas/src/test/controladorTest/ControladorTest.java
@@ -12,8 +12,10 @@ import org.junit.jupiter.api.Test;
 
 import controlador.Controlador;
 import modelo.bbdd.SolicitaCines;
+import modelo.bbdd.SolicitaPeliculas;
 import modelo.pojos.Cine;
 import modelo.pojos.Cliente;
+import modelo.pojos.Pelicula;
 import vista.CajeroEntradas;
 
 class ControladorTest {
@@ -21,6 +23,7 @@ class ControladorTest {
 	Controlador controlador = null;
 	CajeroEntradas cajeroEntradas = null;
 	SolicitaCines solicitaCine = null;
+	SolicitaPeliculas solicitaPeliculas = null;
 
 	@Test
 	void test() {
@@ -37,6 +40,24 @@ class ControladorTest {
 			cajeroEntradas.scCbSeleccionCine.addItem(cines.get(i).getNombre());
 		}
 		assertNotNull(cines);
+	}
+	
+	public void testEscogerCine() {
+		cajeroEntradas = new CajeroEntradas();
+		solicitaPeliculas = new SolicitaPeliculas();
+		String cine = null;
+		ArrayList<Pelicula> pelis = solicitaPeliculas.getPeliculas(
+				"Select Titulo "
+				+ "From Peliculas, Proyecciones, Salas, Cines "
+				+ "Where cines.Nombre = '" + cine + "'and peliculas.codigo = proyecciones.cod_peli and "
+				+ "proyecciones.cod_sala = salas.cod and salas.cod_cine = cines.cod "
+				+ "GROUP BY titulo "
+				+ "ORDER BY FECHA asc, hora ASC");
+		ArrayList<Pelicula> peliculas = solicitaPeliculas.getPeliculas(cine);
+		for (int i = 0; i < pelis.size(); i++) {
+			cajeroEntradas.spCbSeleccionPeli.addItem(pelis.get(i).getTitulo());
+		}
+		
 	}
 
 }
