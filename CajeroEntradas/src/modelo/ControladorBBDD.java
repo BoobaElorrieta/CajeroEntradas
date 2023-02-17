ea56ffa9ea032fe943089507abb32225fa21a808
in1dw3 <in1dw3@HZ352021.elorrieta.int>
2023-02-17 17:04:45 +0100

    movido de controlador a controladorBBDD la funcion comprobar login

--- CajeroEntradas/src/modelo/ControladorBBDD.java
+++ CajeroEntradas/src/modelo/ControladorBBDD.java
@@ -1,23 +1,28 @@
 package modelo;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
+import controlador.Controlador;
 import modelo.bbdd.SolicitaCines;
 import modelo.bbdd.SolicitaCliente;
 import modelo.bbdd.SolicitaHorarios;
 import modelo.bbdd.SolicitaPeliculas;
 import modelo.pojos.Cine;
+import modelo.pojos.Cliente;
 import modelo.pojos.Pelicula;
 import modelo.pojos.Proyeccion;
 import vista.CajeroEntradas;
 
 public class ControladorBBDD {
 
+	Controlador controlador = null;
 	CajeroEntradas cajero = null;
 	SolicitaCines solicitaCine = null;
 	SolicitaPeliculas solicitaPeliculas = null;
@@ -133,6 +138,49 @@ public class ControladorBBDD {
 			horariosLblNombreSala.setText("" + proyecciones.get(i).getSala().getNombre() + "");
 		}
 
+	}
+	
+	public void comprobarLogin(JTextField loginTfEmail, JTextField loginTfContrasena) throws IOException {
+		controlador = new Controlador();
+		String correo = null;
+		String contrasenaUsuario = null;
+		String contrasenaReal = null;
+		int ret;
+		correo = (String) loginTfEmail.getText();
+		contrasenaUsuario = (String) loginTfContrasena.getText();
+		try {
+
+			solicitaClientes = new SolicitaCliente();
+			ArrayList<Cliente> cliente = solicitaClientes
+					.getClientes("SELECT email, contrasena FROM clientes WHERE email = '" + correo + "'");
+			contrasenaReal = cliente.get(0).getContrasena();
+			if (contrasenaUsuario.equalsIgnoreCase(contrasenaReal)) {
+				// Se le deja pasar
+				JFrame jFrame = new JFrame();
+				ret = JOptionPane.showConfirmDialog(null, "ACESO PERMITIDO \r\n" + "Desea imprimir el ticket?",
+						"YES_NO_OPTION", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
+
+				switch (ret) {
+				case 0:
+					String directorio = JOptionPane.showInputDialog("Donde quiere guardar su ticket?");
+					JOptionPane.showMessageDialog(jFrame, "Su ticket " + directorio
+							+ " ha sido guardado en DESCARGAS \r\n" + "Gracias por su compra:)");
+					controlador.crearFichero();
+					break;
+				case 1:
+					JOptionPane.showMessageDialog(jFrame, "Gracias Por su compra:)");
+					break;
+
+				}
+			} else {
+				JFrame jFrame = new JFrame();
+				JOptionPane.showMessageDialog(jFrame, "ACESO DENEGADO");
+			}
+
+		} catch (Exception e) {
+			JFrame jFrame = new JFrame();
+			JOptionPane.showMessageDialog(jFrame, "ERROR, ese email no existe en la base de datos.");
+		}
 	}
 		
 }
